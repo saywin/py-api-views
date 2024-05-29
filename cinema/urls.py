eacb16a502f0989ed0c0526f73f5ff2f17f5a299
tiway <stilon670@gmail.com>
2024-05-29 14:29:39 +0300

    Add endpoints to urls.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import GenreListView, ActorListView, ActorDetailView, GenreDetailView, MovieViewSet
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreListView.as_view(), name="genre_list"),
+    path("genres/<int:pk>/", GenreDetailView.as_view(), name="genre_detail"),
+    path("actors/", ActorListView.as_view(), name="actor_list"),
+    path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor_detail"),
+    path("cinemahalls/", ActorListView.as_view(), name="cinemahalls_list"),
+    path("cinemahalls/<int:pk>/", ActorDetailView.as_view(), name="cinemahalls_detail"),
+    path("", include(router.urls))
 ]
 
 app_name = "cinema"
